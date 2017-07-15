00178edf326eecb6b6eaabf739f48db9b2e5d296
Graeme Lockley <graemel@no9.co.za>
2017-07-15 16:24:26 +0200

    feature: add Maybe to Libs

--- test/Lib.js
+++ test/Lib.js
@@ -3,5 +3,6 @@ const Libs = require("../src/Lib");
 
 module.exports = Object.assign({}, Libs, {
     Assertion: mrequire("core:Test.Unit.Assertion:1.1.0"),
+    Maybe: mrequire("core:Native.Data.Maybe:1.0.0"),
     Unit: mrequire("core:Test.Unit:0.0.1")
 });
