6267f4461d2090b44fd3c0095ad5cdbe7ec9c756
Graeme Lockley <graemel@no9.co.za>
2017-07-15 15:02:06 +0200

    refactor: simplify add by moving out into multiple supporting functions

--- test/sc/SC.js
+++ test/sc/SC.js
@@ -1,8 +1,16 @@
 const Array = mrequire("core:Native.Data.Array:1.0.0");
 
 
+const tokenize = input =>
+    input.split(/[,\n]/);
+
+
+const parse = input =>
+    tokenize(input).map(s => parseInt(s));
+
+
 const add = input =>
-    input === "" ? 0 : Array.sum(input.split(/[,\n]/).map(s => parseInt(s)));
+    input === "" ? 0 : Array.sum(parse(input));
 
 
 module.exports =
