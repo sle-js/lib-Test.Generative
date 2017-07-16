0f28953f3f0fd4a5811aa89c60820e67c24b63f0
Graeme Lockley <graemel@no9.co.za>
2017-07-16 08:09:13 +0200

    feature: add any

--- src/Array.js
+++ src/Array.js
@@ -13,4 +13,10 @@ assumptionEqual([2, 3, 4], Array.drop(1)([1, 2, 3, 4]));
 assumptionEqual([3, 4], Array.drop(2)([1, 2, 3, 4]));
 
 
+Array.any = p => a =>
+    a.some(p);
+assumption(!Array.any(x => x < 0)([1, 2, 3, 4]));
+assumption(Array.any(x => x < 0)([1, 2, -3, 4]));
+
+
 module.exports = Array;
